90d217584fa0f71ce61d4e27ea863bb28200e016
Luiz Motta <33238105+lgcmotta@users.noreply.github.com>
2022-10-24 17:21:25 -0300

    Refactor: Create Module Builder (#1)
    * WIP:  module builder refactor
    
    * WIP: build API config
    
    * fix: usage of API is not required
    
    * refactor: move code responsability
    
    * refactor: move code responsability
    
    * feat: add DI interface and configs
    
    * chore: run go mod tidy
    
    * fix: make API optional
    
    * fix: configure API handler

--- controller.go
+++ controller.go
@@ -1,6 +1,11 @@
 package modules
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/dig"
+)
+
+type ControllerConstructorFunc func(container *dig.Container) IController
 
 type IController interface {
 	Register(group *gin.RouterGroup)
